9a1fcab0713547b44a2722ea12281f198c6421a8
wim <wim@gbor.nl>
2025-11-22 15:45:07 +0100

    meer tests

--- src/test/java/algorithms/stack/ListStackTest.java
+++ src/test/java/algorithms/stack/ListStackTest.java
@@ -4,7 +4,10 @@ import algorithms.linkedlist.ILinkedListOneDirection;
 import algorithms.linkedlist.LinkedListOneDirection;
 import org.junit.Test;
 
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class ListStackTest {
     @Test
@@ -13,8 +16,39 @@ public class ListStackTest {
         for(int i = 0; i < 10000; i++){
             listStack.push(i);
         }
+        listStack.pop();
         int result = listStack.pop();
+        int expected = 9998;
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void PushTop(){
+        IStack<Integer> listStack = new ListStack<Integer>();
+        for(int i = 0; i < 10000; i++){
+            listStack.push(i);
+        }
+        listStack.top();
+        int result = listStack.top();
         int expected = 9999;
         assertEquals(expected, result);
+    }
+
+    @Test
+    public void PopMoreThanExists(){
+        IStack<Integer> listStack = new ListStack<Integer>();
+        for(int i = 0; i < 10000; i++){
+            listStack.push(i);
+        }
+
+        Exception exception = assertThrows(NullPointerException.class, () -> {
+            for(int i = 0; i < 100000; i++){
+                listStack.pop();
+            }
+        });
+
+        String expectedMessage = "\"segmentToBeRemoved\" is null";
+        String actualMessage = exception.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
     }
 }
