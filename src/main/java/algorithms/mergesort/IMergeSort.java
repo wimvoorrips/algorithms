34b9b9000caad5a988dabecdbed9b64757597884
wim <wim@gbor.nl>
2025-11-27 01:57:34 +0100

    mergesort werkt

--- src/main/java/algorithms/mergesort/IMergeSort.java
+++ src/main/java/algorithms/mergesort/IMergeSort.java
@@ -1,4 +1,5 @@
 package algorithms.mergesort;
 
 public interface IMergeSort <T> {
+    T[] sort(Class<T> clazz, T[] arrayToMergeSort);
 }
--- src/main/java/algorithms/mergesort/MergeSort.java
+++ src/main/java/algorithms/mergesort/MergeSort.java
@@ -5,41 +5,60 @@ import java.util.Arrays;
 
 public class MergeSort <T> implements IMergeSort <T> {
 
-    MergeSort(){
+    public MergeSort(){
 
     }
 
-    public T[] sort(T[] arrayToMergeSort){
+    public T[] sort(Class<T> clazz, T[] arrayToMergeSort){
+        //System.out.println("lengte");
+        //System.out.println(arrayToMergeSort.length);
+
         if(arrayToMergeSort.length < 2){
             return arrayToMergeSort;
         }
 
         int middle = arrayToMergeSort.length / 2;
+        //System.out.println(middle);
+
         T[] firstSet = Arrays.copyOfRange(arrayToMergeSort, 0, middle);
-        T[] secondSet = Arrays.copyOfRange(arrayToMergeSort, middle + 1, arrayToMergeSort.length);
+        T[] secondSet = Arrays.copyOfRange(arrayToMergeSort, middle, arrayToMergeSort.length);
 
-        T[] sortedFirstSet = this.sort(firstSet);
-        T[] sortedSecondSet = this.sort(secondSet);
+        //System.out.println(firstSet[0]);
+        //System.out.println(secondSet[0] +","+secondSet[1]);
 
-        return merge(sortedFirstSet, sortedSecondSet);
+        T[] sortedFirstSet = this.sort(clazz, firstSet);
+        T[] sortedSecondSet = this.sort(clazz, secondSet);
+
+        return merge(clazz, sortedFirstSet, sortedSecondSet);
 
     }
 
-    private T[] merge(T[] firstArray, T[] secondArray){
+    private T[] merge(Class<T> clazz, T[] firstArray, T[] secondArray){
+        //System.out.println("merge lengte " + firstArray.length + " en " + secondArray.length);
+        //System.out.println(firstArray.length);
+        //System.out.println(secondArray.length);
 
         Comparable<T>[] firstComparableArray = arrayToComparable(firstArray);
 
-        T[] mergedArray = (T[]) new Comparable[firstArray.length + secondArray.length];
-
+        //T[] mergedArray = (T[]) new Comparable[firstArray.length + secondArray.length];
+        //T[] mergedArray = (T[]) Array.newInstance(,[firstArray.length + secondArray.length];
+        T[] mergedArray = (T[]) Array.newInstance(clazz, firstArray.length + secondArray.length);
 
         int firstArrayIndex = 0;
         int secondArrayIndex = 0;
         int mergedArrayIndex = 0;
 
-        while (mergedArrayIndex <= mergedArray.length){
+        //System.out.println("lengte van mergedArray: " + mergedArray.length);
+
+        while (mergedArrayIndex < mergedArray.length){
+            //System.out.println("index:" + firstArrayIndex);
             if(
-                firstArrayIndex <= firstArray.length &&
-                firstComparableArray[firstArrayIndex].compareTo( secondArray[secondArrayIndex] ) <= 0
+                firstArrayIndex < firstArray.length &&
+                (
+                        secondArrayIndex >= secondArray.length ||
+                        firstComparableArray[firstArrayIndex].compareTo( secondArray[secondArrayIndex] ) <= 0
+                )
+
             ) {
                 mergedArray[mergedArrayIndex] = firstArray[firstArrayIndex];
                 firstArrayIndex += 1;
@@ -47,6 +66,8 @@ public class MergeSort <T> implements IMergeSort <T> {
                 mergedArray[mergedArrayIndex] = secondArray[secondArrayIndex];
                 secondArrayIndex += 1;
             }
+            //System.out.println(mergedArrayIndex);
+            //System.out.println(mergedArray[mergedArrayIndex]);
             mergedArrayIndex += 1;
         }
 
